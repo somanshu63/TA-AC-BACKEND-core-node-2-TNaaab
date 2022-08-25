a0d14dbf46e4d8f7609dba602df87a7aadfa0203
somanshu63 <somanshu63@gmail.com>
2022-08-25 13:37:10 +0530

    node 2 assignment 2

--- block-BNaabf/server.js
+++ block-BNaabf/server.js
@@ -13,21 +13,30 @@ console.log(__dirname + '/index.js');
 var server = http.createServer(handleRequest);
 
 function handleRequest(req, res) {
-    var parsedUrl = url.parse(req.url);
-    if(req.method == 'GET' && parsedUrl.pathname == '/form'){
-        fs.createReadStream('./form.html').pipe(res);
-    }
-    if(req.method == 'POST' && parsedUrl.pathname == '/form'){
-        var store = '';
-        req.on('data', (chunk) => {
-            store += chunk;
-        });
-        req.on('end', () => {
+    var parsedUrl = url.parse(req.url, true);
+    console.log(req.method, parsedUrl.pathname);
+
+    var store = '';
+    req.on('data', (chunk) => {
+        store += chunk;
+    });
+    req.on('end', () => {
+        if(req.method == 'GET' && parsedUrl.pathname == '/form'){
+            fs.readFile('./form.html', (err, content) => {
+                res.end(content);
+            });
+        }
+        if(req.method == 'POST' && parsedUrl.pathname == '/form'){
+            res.setHeader('content-type', 'text/html');
             var parsedData = querystring.parse(store);
-            res.write(JSON.stringify(parsedData));
+            res.write(`<h2>${parsedData.name}</h2>`)
+            res.write(`<h3>${parsedData.email}</h3>`)
+            res.write(`<h3>${parsedData.age}</h3>`)
             res.end();
-        });
-    }
+        }
+    });
+    
+    
 }
 
 server.listen(5678, () => {
